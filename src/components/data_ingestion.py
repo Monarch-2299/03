8da23ed2bd3c08e4f70cd73c1317ebba2a0c0b9c
anil.pokharel@eastern.edu <anil.pokharel@eastern.edu>
2024-03-07 14:16:20 -0700

    commited

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -49,4 +51,6 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
+
+    
    
